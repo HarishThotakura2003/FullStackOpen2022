2d275e62eeff350f5488bf3dbdd20b6bbca6fb00
HarishThotakura2003 <harishthotakura2003@gmail.com>
2022-07-22 02:00:15 +0000

    Completed Exercise 1.10 of part1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 
+const Button = ({text,onClick}) =>{
+  return(
+    <button onClick={onClick} >
+      {text}
+    </button>
+  )
+}
+
 const Statistics = ({good,neutral,bad}) =>{
-  if(good==0&&neutral==0&&bad==0){return(
+  if(good===0&&neutral===0&&bad===0){return(
     <>
     <p>No feedback given </p>
     </>
@@ -29,9 +37,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() =>{setGood(good+1);}}>good</button>
-      <button onClick={() =>{setNeutral(neutral+1);}}>neutral</button>
-      <button onClick={() =>{setBad(bad+1);}}>bad</button>
+      <Button text="good" onClick={()=>{setGood(good+1)}} />
+      <Button text="neutral" onClick={()=>{setNeutral(neutral+1)}} />
+      <Button text="bad" onClick={()=>{setBad(bad+1)}} />
       <h1>statisics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />      
     </div>
