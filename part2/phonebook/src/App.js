c5be83cdd66255d875f144eb7c4633b3eb80a1b8
HarishThotakura2003 <harishthotakura2003@gmail.com>
2022-07-23 07:46:23 +0000

    Completed 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,20 +6,23 @@ const Header =({text}) =>{
   )
 }
 
-const Person =({name})=>{
+const Person =({name,number})=>{
   return(
     <>
-    <p>{name}</p>
+    <p>{name}: {number}</p>
     </>
   )
 }
 
-const Form = ({input,onSubmit,onInput}) =>{
+const Form = ({onSubmit,name,number}) =>{
   return(
     <>
     <form onSubmit={onSubmit}>
+    <div>
+        name: <input onChange={name}/>
+      </div>
       <div>
-        name: <input value={input} onChange={onInput}/>
+        number: <input onChange={number}/>
       </div>
       <div>
         <button type='submit'>add</button>
@@ -31,33 +34,37 @@ const Form = ({input,onSubmit,onInput}) =>{
 
 const App = () => {
   const [persons,setPersons] = useState(
-    [{name:'Arto Hellas '}]
+    [{name:'Arto Hellas ',number:'040-1234567'}]
     )
-  const [input,setInput] = useState('')
-  
-  const inputChange = (event) => {
-    setInput(event.target.value)
-  }
 
-  const addNumber=(event) => {
-    event.preventDefault()
-    const newNumber={
-      name:input
+  const [name,setName] = useState('')
+  const [number,setNumber] = useState('')
+  const handleNameChange = (e) => {setName(e.target.value);}
+  const handleNumberChange = (e) => {setNumber(e.target.value);}
+
+  const submit = (e) => {
+    e.preventDefault();
+    const n = {
+      name:name,
+      number:number,
     }
-    if(persons.some(person => person.name === newNumber.name)){
-      alert(`${newNumber.name} is already added to phonebook`)
+    if (persons.some(person=>person.name==n.name)){
+      alert(`${n.name} is already added to phonebook`)
     }
-    else{setPersons(persons.concat(newNumber))}
-    
-    
+    else{
+      setPersons(persons.concat(n))
+      setName('')
+      setNumber('')
+    }
   }
+  
 
   return (
     <div>
       <Header text="Phonebook" />
-      <Form input={input} onSubmit={addNumber} onInput={inputChange}/>
+      <Form onSubmit={submit} name={handleNameChange} number={handleNumberChange}/>
       <Header text="Numbers" />
-      {persons.map(person=><Person name={person.name} />)}
+      {persons.map(person=><Person name={person.name} number={person.number}/>)}
     </div>
   )
 }
