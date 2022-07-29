08c0797248357feb639d5bd403732ac194539ecb
HarishThotakura2003 <harishthotakura2003@gmail.com>
2022-07-29 02:42:31 +0000

    Added morgan format

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,7 +9,16 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 app.use(express.static('build'))
 
 const PORT = process.env.PORT || 3001
