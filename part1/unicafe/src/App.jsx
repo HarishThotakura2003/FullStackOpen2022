7e67b1dd001e111c5a978d94a8c7ba6a5c8b078b
HarishThotakura2003 <102403284+HarishThotakura2003@users.noreply.github.com>
2022-12-29 20:23:40 +0530

    Completed Ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,23 @@ const Button = ({handleClick,text}) =>{
   )
 }
 
+const Statistics = ({good,bad,neutral}) => {
+  const total = good+bad+neutral;
+  const average = (good-bad)/total;
+  const positive = good/total;
+  return(
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good,setGood] = useState(0);
   const [neutral,setNeutral] = useState(0);
@@ -16,13 +33,7 @@ const App = () => {
       <Button handleClick={()=>setGood(good+1)} text={"good"} />
       <Button handleClick={()=>setNeutral(neutral+1)} text={"neutral"} />
       <Button handleClick={()=>setBad(bad+1)} text={"bad"} />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+bad+neutral}</p>
-      <p>average {(good-bad)/(good+bad+neutral)}</p>
-      <p>positive {good/(good+bad+neutral) * 100} %</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
     
   )
