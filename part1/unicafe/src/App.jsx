3a48fa6eb3869260f60eb874c6305a8bea1f315d
HarishThotakura2003 <102403284+HarishThotakura2003@users.noreply.github.com>
2022-12-29 20:15:04 +0530

    Completed 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+const Button = ({handleClick,text}) =>{
+  return(
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 
 const App = () => {
+  const [good,setGood] = useState(0);
+  const [neutral,setNeutral] = useState(0);
+  const [bad,setBad] = useState(0);
   return (
-    <div>App</div>
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={()=>setGood(good+1)} text={"good"} />
+      <Button handleClick={()=>setNeutral(neutral+1)} text={"neutral"} />
+      <Button handleClick={()=>setBad(bad+1)} text={"bad"} />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+    
   )
 }
 
